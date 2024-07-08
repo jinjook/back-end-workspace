a3f0e7f629507249a113050165df51990193d254
jinjook <kjj_21@naver.com>
2024-07-08 10:40:16 +0900

    java - 객체 연습문제 풀이

--- 2_Java/07_Inheritance/src/com/kh/example/practice3/Application.java
+++ 2_Java/07_Inheritance/src/com/kh/example/practice3/Application.java
@@ -7,19 +7,123 @@ import com.kh.example.practice3.model.Employee;
 
 public class Application {
 
+	Scanner sc = new Scanner(System.in);
+	Employee viewEmployee = new Employee();
+	EmployeeController employeeController = new EmployeeController();
 	
+	int empNo = 0;
+	String name = "";
+	char gender = 0;
+	String phone = "";
+	String dept = "";
+	int salary = 0;
+	double bonus = 0;
 	
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-		Application a = new Application();
-		Employee viewEmployee = new Employee();
-		EmployeeController employeeController = new EmployeeController();
+		Application app = new Application();
+		boolean check = true;
+		while(check) {
+			int select = app.employeeMenu();
+			switch(select) {
+				case 1:
+					app.insertEmp();
+					break;
+				case 2:
+					app.updateEmp();
+					break;
+				case 3: 
+					app.printEmp();
+					break;
+				case 9: 
+					System.out.println("프로그램을 종료합니다.");
+					check = false;
+					break;
+				default : 
+					System.out.println("잘못 입력하셨습니다. 다시 입력해주세요");
+			}
+		}
 		
+	}
 		
+	// 메인 메뉴를 출력하는 method
+	public int employeeMenu() {
+		System.out.println("1. 사원 정보 추가");
+		System.out.println("2. 사원 정보 수정");
+		System.out.println("3. 사원 정보 출력");
+		System.out.println("9. 프로그램 종료");
+		System.out.print("메뉴 번호를 누르세요 : ");
+		return Integer.parseInt(sc.nextLine());
+	}
 		
+	// 저장할 데이터를 사용자에게 받는 method
+	// viewEmployee 사용 -> javascript로 바꿀때 이용..?
+	public void insertEmp() {
+		System.out.print("사원 번호 : ");
+//		empNo = Integer.parseInt(sc.nextLine());
+		viewEmployee.setEmpNo(Integer.parseInt(sc.nextLine()));
+		System.out.print("사원 이름 : ");
+//		name = sc.nextLine();
+		viewEmployee.setName(sc.nextLine());
+		System.out.print("사원 성별 : ");
+//		gender = sc.nextLine().charAt(0);
+		viewEmployee.setGender(sc.nextLine().charAt(0));
+		System.out.print("전화 번호 : ");
+//		phone = sc.nextLine();
+		viewEmployee.setPhone(sc.nextLine());
+		System.out.print("추가 정보를 더 입력하시겠습니까?(y/n) : ");
+		char add = sc.nextLine().charAt(0);
+		if (add == 'y') {
+			System.out.print("사원 부서 : ");
+			dept = sc.nextLine();
+			System.out.print("사원 연봉 : ");
+			salary = Integer.parseInt(sc.nextLine());
+			System.out.print("보너스 율 : ");
+			bonus = Double.parseDouble(sc.nextLine());
+//			employeeController.add(empNo, name, gender, phone, dept, salary, bonus);
+			employeeController.add(viewEmployee.getEmpNo(), viewEmployee.getName(), viewEmployee.getGender(), viewEmployee.getPhone(), viewEmployee.getDept(), viewEmployee.getSalary(), viewEmployee.getBonus());
+		} else {
+//			employeeController.add(empNo, name, gender, phone);
+			employeeController.add(viewEmployee.getEmpNo(), viewEmployee.getName(), viewEmployee.getGender(), viewEmployee.getPhone());
+		}
+	}
 		
-		while(true) {
+
+	// 수정할 데이터를 사용자에게 받는 method
+	public void updateEmp() {
+		System.out.println("사원의 어떤 정보를 수정하시겠습니까?");
+		System.out.println("1. 전화 번호");
+		System.out.println("2. 사원 연봉");
+		System.out.println("3. 보너스 율");
+		System.out.println("9. 돌아가기");
+		System.out.print("메뉴 번호를 누르세요 : ");
+		int num = Integer.parseInt(sc.nextLine());
+		if(num == 1) { // switch로도 가능
+			System.out.print("전화 번호 입력 : ");
+			phone = sc.nextLine();
+			employeeController.modify(phone);
+		} else if (num == 2) {
+			System.out.print("사원 연봉 입력 : ");
+			salary = Integer.parseInt(sc.nextLine());
+			employeeController.modify(salary);
+		} else if (num == 3) {
+			System.out.print("보너스 율 입력 : ");
+			bonus = sc.nextDouble();
+			sc.nextLine();
+			employeeController.modify(bonus);
+	}
+				
+	}
+			
+	// 데이터를 출력하는 method
+	public void printEmp() {
+		System.out.println(employeeController.info());
+	}
+			
+			
+			
+		/* 내가 짠 코드
+		 * while(true) {
 			System.out.println("1. 사원 정보 추가");
 			System.out.println("2. 사원 정보 수정");
 			System.out.println("3. 사원 정보 출력");
@@ -31,25 +135,28 @@ public class Application {
 			String name = "";
 			char gender = 0;
 			String phone = "";
+			String dept = "";
+			int salary = 0;
+			double bonus = 0;
 			
 			if(num == 1) {	
 				System.out.print("사원 번호 : ");
-				int empNo = Integer.parseInt(sc.nextLine());
+				empNo = Integer.parseInt(sc.nextLine());
 				System.out.print("사원 이름 : ");
-				String name = sc.nextLine();
+				name = sc.nextLine();
 				System.out.print("사원 성별 : ");
-				char gender = sc.nextLine().charAt(0);
+				gender = sc.nextLine().charAt(0);
 				System.out.print("전화 번호 : ");
-				String phone = sc.nextLine();
+				phone = sc.nextLine();
 				System.out.print("추가 정보를 더 입력하시겠습니까?(y/n) : ");
 				char yn = sc.nextLine().charAt(0);
 				if (yn == 'y') {
 					System.out.print("사원 부서 : ");
-					String dept = sc.nextLine();
+					dept = sc.nextLine();
 					System.out.print("사원 연봉 : ");
-					int salary = Integer.parseInt(sc.nextLine());
+					salary = Integer.parseInt(sc.nextLine());
 					System.out.print("보너스 율 : ");
-					double bonus = sc.nextDouble();
+					bonus = sc.nextDouble();
 					sc.nextLine();
 				}
 			} else if (num == 2) {
@@ -62,77 +169,27 @@ public class Application {
 				int num2 = Integer.parseInt(sc.nextLine());
 				if(num2 == 1) {
 					System.out.print("전화 번호 입력 : ");
-					String phone = sc.nextLine();
+					phone = sc.nextLine();
 				} else if (num2 == 2) {
 					System.out.println("사원 연봉 입력 : ");
-					int updateSalary = Integer.parseInt(sc.nextLine());
+					salary = Integer.parseInt(sc.nextLine());
 				} else if (num2 == 3) {
 					System.out.println("보너스 율 입력 : ");
-					double updateBonus = sc.nextDouble();
+					bonus = sc.nextDouble();
 					sc.nextLine();
 				}
 			} else if (num == 3) {
 				employeeController.add(empNo, name, gender, phone, dept, salary, bonus);
-				System.out.println(viewEmployee);
+				System.out.println(employeeController.info());
 			} else if (num == 9) {
 				System.out.println("프로그램을 종료합니다.");
 				break;
 			}
-	}
+	}*/
 
 }
-/*	
-public int employeeMenu() {
-	System.out.println("1. 사원 정보 추가");
-	System.out.println("2. 사원 정보 수정");
-	System.out.println("3. 사원 정보 출력");
-	System.out.println("9. 프로그램 종료");
-	System.out.print("메뉴 번호를 누르세요 : ");
-	int num = Integer.parseInt(sc.nextLine());
-	return num;
-}
-
-public void insertEmp() {
-		System.out.print("사원 번호 : ");
-		int empNo = Integer.parseInt(sc.nextLine());
-		System.out.print("사원 이름 : ");
-		String name = sc.nextLine();
-		System.out.print("사원 성별 : ");
-		char gender = sc.nextLine().charAt(0);
-		System.out.print("전화 번호 : ");
-		String phone = sc.nextLine();
-		System.out.print("추가 정보를 더 입력하시겠습니까?(y/n) : ");
-		char yn = sc.nextLine().charAt(0);
-		if (yn == 'y') {
-				System.out.print("사원 부서 : ");
-				String dept = sc.nextLine();
-				System.out.print("사원 연봉 : ");
-				int salary = Integer.parseInt(sc.nextLine());
-				System.out.print("보너스 율 : ");
-				double bonus = sc.nextDouble();
-				sc.nextLine();
-		}
-
-public void updateEmp() {
-	System.out.println("사원의 어떤 정보를 수정하시겠습니까?");
-	System.out.println("1. 전화 번호");
-	System.out.println("2. 사원 연봉");
-	System.out.println("3. 보너스 율");
-	System.out.println("9. 돌아가기");
-	System.out.print("메뉴 번호를 누르세요 : ");
-	int num2 = Integer.parseInt(sc.nextLine());
-	if(num2 == 1) {
-		System.out.print("전화 번호 입력 : ");
-		String phone = sc.nextLine();
-	} else if (num2 == 2) {
-		System.out.println("사원 연봉 입력 : ");
-		int updateSalary = Integer.parseInt(sc.nextLine());
-	} else if (num2 == 3) {
-		System.out.println("보너스 율 입력 : ");
-		double updateBonus = sc.nextDouble();
-		sc.nextLine();
-}*/
 
 
 
-}
+
+
--- 2_Java/07_Inheritance/src/com/kh/example/practice3/controller/EmployeeController.java
+++ 2_Java/07_Inheritance/src/com/kh/example/practice3/controller/EmployeeController.java
@@ -4,8 +4,9 @@ import com.kh.example.practice3.model.Employee;
 
 public class EmployeeController {
 	
-	Employee employee = new Employee();
+	private Employee employee = new Employee();
 	
+	// 매개변수 갯수가 다르거나, 타입이 다른 경우 -> 메서드 명을 동일하게 사용가능 : 오버로딩 
 	public void add(int empNo, String name, char gender, String phone) {
 		employee.setEmpNo(empNo);
 		employee.setName(name);
@@ -35,5 +36,9 @@ public class EmployeeController {
 		employee.setBonus(bonus);
 	}
 
-
+	public Employee info() {
+		return employee;
+		// 개발중일때(뭘 return할지 아직 모를때)는 return null;로 초기화해두면 에러 안생김
+		// String도 클래스 -> 클래스를 리턴타입으로 설정할 수 있음
+	}
 }
